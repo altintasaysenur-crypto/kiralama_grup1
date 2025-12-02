8c8ca1661be363fd8279271b39ab7b8ad083f100
aysenuraltintas <ayshenuraltintas@gmail.com>
2025-12-02 10:22:56 +0300

    Proje klasör yapısı eklendi

---
+++ src/models/customer.py
@@ -0,0 +1,8 @@
+class Customer:
+    def __init__(self, id, name, phone):
+        self.id = id
+        self.name = name
+        self.phone = phone
+
+    def to_line(self):
+        return f"{self.id};{self.name};{self.phone}\n"
